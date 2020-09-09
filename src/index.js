576367bb9106ad6df03ac4e31d0449610d194896
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-09 12:25:29 +0900

    Feat: add OrbitControls for controlling camera angle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 let camera, scene, renderer;
+let controls;
 
 init();
-render();
+animate();
 
 function init() {
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
@@ -20,6 +22,19 @@ function init() {
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+
+  controls.minDistance = 100;
+  controls.maxDistance = 2000;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  render();
 }
 
 function render() {
