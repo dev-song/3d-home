d4c11e5de95b63d330f721af15f2a7c0d43a0dcf
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-13 23:59:22 +0900

    Feat: add a navigation for the app

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/navigation.css';
+
+function Navigation() {
+  const linkList = ['About', 'Portfolio', 'Contact', 'Guestbook']
+
+  return (
+    <nav className='navigation'>
+      <ul className='nav__container'>
+        {linkList.map(linkName => <li classNmae='nav__link'>{linkName}</li>)}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
