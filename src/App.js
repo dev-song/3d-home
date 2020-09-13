9e0f4f49d31431d31d7d3ff714bf1c2a8529d169
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-13 23:59:39 +0900

    Refactor: add new components and remove an unnecessary header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import System from './components/3d-system';
+import Navigation from './components/Navigation';
+import System3d from './components/System3d';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <System />
+      <Navigation />
+      <System3d />
     </div>
   );
 }
