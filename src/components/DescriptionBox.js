62fc7e9bc74f583e20dda7ab82fb628d3f73352e
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-14 23:56:52 +0900

    Feat: add a DescriptionBox component for displaying descriptions for each link

---
+++ src/components/DescriptionBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DescriptionBox({ title }) {
+  return (
+    <article className='description-box'>
+      {title}
+    </article>
+  )
+}
+
+export default DescriptionBox;
