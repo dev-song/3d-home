efacb7488b194b53e9515ee3159d2dd4e8a45d81
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-16 09:54:31 +0900

    Feat: DescriptionBox component now contains more detailed info

--- src/components/DescriptionBox.js
+++ src/components/DescriptionBox.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './DescriptionBox.css';
 
-function DescriptionBox({ title }) {
+function DescriptionBox({ content }) {
+  const { title, subtitle, body } = content;
+
   return (
     <article className='description-box'>
-      {title}
+      <h2 className='description__title'>{title}</h2>
+      <h4 className='description__subtitle'>{subtitle}</h4>
+      <p className='description__body'>{body}</p>
     </article>
   )
 }
