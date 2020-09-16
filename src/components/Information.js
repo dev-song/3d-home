44bec482180e40613587ad45a2acb1b01c422068
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-16 09:54:08 +0900

    Feat: add a new data class, Content

--- src/components/Information.js
+++ src/components/Information.js
@@ -2,11 +2,25 @@ import React from 'react';
 import Navigation from './Navigation';
 import DescriptionBox from './DescriptionBox';
 
+class Content {
+  constructor(title, subtitle, body) {
+    this.title = title;
+    this.subtitle = subtitle;
+    this.body = body;
+  }
+}
+
 class Information extends React.Component {
   constructor() {
     super();
     this.state = {
-      selectedLink: null
+      selectedLink: null,
+      contentByMenu: {
+        About: new Content('About', 'About-sub', 'Body'),
+        Portfolio: new Content('Portfolio', 'Portfolio-sub', 'Body'),
+        Contact: new Content('Contact', 'Contact-sub', 'Body'),
+        Guestbook: new Content('Guestbook', 'Guestbook-sub', 'Body')
+      }
     };
 
     this.handleStateChange = this.handleStateChange.bind(this);
@@ -18,12 +32,16 @@ class Information extends React.Component {
 
   render() {
     const state = this.state;
+    console.log(state.contentByMenu[state.selectedLink]);
 
     return (
       <main role='main'>
         <Navigation handleStateChange={this.handleStateChange} />
         {state.selectedLink
-          ? <DescriptionBox title={this.state.selectedLink} />
+          ?
+          <DescriptionBox
+            content={state.contentByMenu[state.selectedLink]}
+          />
           : null
         }
       </main>
