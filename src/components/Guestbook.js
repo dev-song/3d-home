376d75c24712ead96d4d479c74e6977e6eaee620
dev_song <52951039+dev-song@users.noreply.github.com>
2020-09-20 00:30:48 +0900

    Refactor: change functions to class methods

--- src/components/Guestbook.js
+++ src/components/Guestbook.js
@@ -4,57 +4,75 @@
  */
 
 import React from 'react';
-import { initializeApp, database } from 'firebase/app';
+import { database } from 'firebase/app';
 import 'firebase/database';
 
-function readFirebaseData(category) {
-  database().ref(category).on('value', (snapshot) => {
-    console.log(snapshot.val());
-  });
-}
-
-function saveToFirebase(data, category) {
-  database().ref(category).push(data)
-    .then(
-      function (snapshot) {   // executed on success
-        console.log(`Data is successfully written`);
-      }, function (error) {   // executed on error
-        console.error(`Error: ${error}`);
-      }
-    );
-}
-
-function handleGuestbookSubmit(event) {
-  event.preventDefault();
-
-  const guestbookData = {
-    regTime: new Date().getTime(),
-    name: event.target.querySelector('#name').value,
-    password: event.target.querySelector('#password').value,
-    message: event.target.querySelector('#message').value
-  }
-
-  saveToFirebase(guestbookData, 'guestbook');
-}
+const TARGET_DB = 'guestbook';
 
 class Guestbook extends React.Component {
-  componentDidMount() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBK8K7qYrjLgRHUIBafGSENfMhl0FpdeWE",
-      authDomain: "home-guestbook.firebaseapp.com",
-      databaseURL: "https://home-guestbook.firebaseio.com",
-      projectId: "home-guestbook",
-      storageBucket: "home-guestbook.appspot.com",
-      messagingSenderId: "142799144544",
-      appId: "1:142799144544:web:001250f60df6f2b396a505",
-      measurementId: "G-67LGRCK27G"
-    };
-    initializeApp(firebaseConfig);
+  constructor() {
+    super();
+    this.state = {
+      guestbook: null
+    }
+
+    this.loadGuestbookData = this.loadGuestbookData.bind(this);
+    this.postGuestbookData = this.postGuestbookData.bind(this);
+    this.handleGuestbookSubmit = this.handleGuestbookSubmit.bind(this);
   }
+
+  loadGuestbookData(category) {
+    database().ref(category).on('value', (snapshot) => {
+      if (snapshot.val().length < 1) {
+        console.log(`There's no guestbook data.`);
+        return;
+      }
+
+      const guestbook = [];
+      snapshot.forEach(childSnapshot => {
+        const guestbookItem = {
+          key: childSnapshot.key,
+          data: childSnapshot.val()
+        }
+        guestbook.push(guestbookItem);
+      });
+
+      this.setState({ guestbook: guestbook });
+    });
+  }
+
+  postGuestbookData(data, category) {
+    database().ref(category).push(data)
+      .then(
+        function (snapshot) {   // executed on success
+          console.log(`Data is successfully written`);
+        }, function (error) {   // executed on error
+          console.error(`Error: ${error}`);
+        }
+      );
+  }
+
+  handleGuestbookSubmit(event) {
+    event.preventDefault();
+
+    const guestbookData = {
+      regTime: new Date().getTime(),
+      name: event.target.querySelector('#name').value,
+      password: event.target.querySelector('#password').value,
+      message: event.target.querySelector('#message').value
+    }
+
+    this.postGuestbookData(guestbookData, TARGET_DB);
+  }
+
+  componentDidMount() {
+    this.loadGuestbookData(TARGET_DB);
+  }
+
   render() {
     return (
       <div className='guestbook'>
-        <form className='guestbook-form' onSubmit={handleGuestbookSubmit}>
+        <form className='guestbook-form' onSubmit={this.handleGuestbookSubmit}>
           <input className='guestbook-form__name' id='name' type='text' placeholder='Name' />
           <input className='guestbook-form__password' id='password' type='password' placeholder='Password' />
           <input className='guestbook-form__message' id='message' type='text' placeholder='Message' />
