51967871798e1794473b27ad7edf5f48baed946d
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-26 23:57:52 +0900

    Update about.js
    - Add a Korean translation

--- src/data/about.js
+++ src/data/about.js
@@ -21,4 +21,19 @@ const ABOUT_INFO = {
   ]
 }
 
+const ABOUT_INFO_KR = {
+  username: 'dev-song',
+  status: '웹 개발자 꿈나무',
+  intro: "매일매일 한 걸음씩 나아가는 중",
+  skills: [
+    new Skill('HTML', 70, '중급'),
+    new Skill('CSS', 70, '중급'),
+    new Skill('JavaScript', 40, '중급'),
+    new Skill('React', 20, '초급'),
+    new Skill('NodeJS', 5, '입문'),
+    new Skill('ExpressJS', 5, '입문'),
+    new Skill('MySQL', 5, '입문')
+  ]
+}
+
 export default ABOUT_INFO;
