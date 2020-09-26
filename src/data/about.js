be91e45012b0b03698bc75384c9c43b82af7901a
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-27 00:13:30 +0900

    Refactor: modify a structure for Korean translation

--- src/data/about.js
+++ src/data/about.js
@@ -18,22 +18,10 @@ const ABOUT_INFO = {
     new Skill('NodeJS', 5, 'Starter'),
     new Skill('ExpressJS', 5, 'Starter'),
     new Skill('MySQL', 5, 'Starter')
-  ]
-}
+  ],
 
-const ABOUT_INFO_KR = {
-  username: 'dev-song',
-  status: '웹 개발자 꿈나무',
-  intro: "매일매일 한 걸음씩 나아가는 중",
-  skills: [
-    new Skill('HTML', 70, '중급'),
-    new Skill('CSS', 70, '중급'),
-    new Skill('JavaScript', 40, '중급'),
-    new Skill('React', 20, '초급'),
-    new Skill('NodeJS', 5, '입문'),
-    new Skill('ExpressJS', 5, '입문'),
-    new Skill('MySQL', 5, '입문')
-  ]
+  statusKR: '웹 개발자 꿈나무',
+  introKR: "매일매일 한 걸음씩 나아가는 중",
 }
 
 export default ABOUT_INFO;
