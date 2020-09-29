f61daa2419dcfd792123f65d36e343e59dc36398
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-29 12:26:14 +0900

    Fix: modify content of About component

--- src/data/about.js
+++ src/data/about.js
@@ -8,8 +8,8 @@ class Skill {
 
 const ABOUT_INFO = {
   username: 'dev-song',
-  status: 'A web developer wannabe',
-  intro: "I'm seeking for a better future.",
+  status: 'Web front-end developer',
+  intro: "When working in HR department after graduating Economics major at the university, I was attracted by the vision of software development and started studying. I wish to be steady, improving little by little everyday.",
   skills: [
     new Skill('HTML', 70, 'Skilled'),
     new Skill('CSS', 70, 'Skilled'),
@@ -20,8 +20,8 @@ const ABOUT_INFO = {
     new Skill('MySQL', 5, 'Starter')
   ],
 
-  statusKR: '웹 개발자 꿈나무',
-  introKR: "매일매일 한 걸음씩 나아가는 중",
+  statusKR: '웹 프론트엔드 개발자',
+  introKR: "경제학과를 졸업하여 인사 관련 일을 하던 중, 개발자의 길에 매력을 느껴 전업을 위해 공부중입니다. 매일매일 배우며 한 걸음씩 꾸준히 나아가는 개발자가 되고자 합니다.",
 }
 
 export default ABOUT_INFO;
